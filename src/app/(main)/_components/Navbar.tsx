b326d77f13eb34bf3b2b89e716426bdb0019f9ed
harshOpensource <harshopensource@gmail.com>
2023-12-31 18:10:17 +0530

    publish done

--- src/app/(main)/_components/Navbar.tsx
+++ src/app/(main)/_components/Navbar.tsx
@@ -7,6 +7,7 @@ import { MenuIcon } from "lucide-react";
 import Title from "./Title";
 import Banner from "./Banner";
 import Menu from "./Menu";
+import { Publish } from "./Publish";
 
 type Props = {
   isCollapsed: boolean;
@@ -47,6 +48,7 @@ function Navbar({ isCollapsed, onResetWidth }: Props) {
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
